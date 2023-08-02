efbacec586f684460db593f2f6386e09fdf34e23
David Chavez <depch47@gmail.com>
2023-08-01 21:52:57 -0500

    Add singleton pattern

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import { Database, Government } from "./patterns/creational/singleton";
 
 
-console.log("test")
+// Singleton Pattern
+const government = new Government("Colombian")
+const government2 = new Government("Argentinian")
+
+// console.log(government)
+// console.log(government2)
+
+const db = Database.getInstance()
+const db2 = Database.getInstance()
+
+// console.log(db)
+// console.log(db2)
+
--- src/patterns/creational/singleton.ts
+++ src/patterns/creational/singleton.ts
@@ -1,3 +1,40 @@
 
 
 
+export class Government {
+    private static instance: Government;
+    private _name: string
+    private counter = 0
+    constructor(name: string) {
+        this.counter++
+        this._name = name
+
+        if (Government.instance) {
+            return Government.instance
+        }
+
+        Government.instance = this
+        return this
+    }
+}
+
+
+export class Database {
+    private static instance: Database
+    private _name = "PostgreSQL"
+    private _version = "1.0.2"
+    private counter = 0
+    private constructor() {
+        this.counter++
+    }
+
+    public static getInstance(): Database {
+        if (Database.instance) {
+            return Database.instance
+        }
+        Database.instance = new Database()
+        return Database.instance
+    }
+}
+
+
